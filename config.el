b75e476a192be1ef6c79470c3916a7ad9527c901
Anders Samsø Birch <anders.s.birch1@gmail.com>
2021-06-09 11:30:17 +0200

    Added additional org babel langs

--- config.el
+++ config.el
@@ -66,6 +66,13 @@
 
 (setq evil-move-cursor-back nil)
 
-
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (sh . t)
+   (elixir . t)
+   (org . t)
+   (ditaa . t)
+   (clojure . t)))
 ;(after! org
  ; (setq org-startup-indented nil))
