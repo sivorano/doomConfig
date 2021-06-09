42f04e6a569a98d929d45149f11436a191d4ec97
Anders Samsø Birch <anders.s.birch1@gmail.com>
2021-06-09 11:30:35 +0200

    Added hy support

--- init.el
+++ init.el
@@ -132,7 +132,7 @@
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
-       ;;hy                ; readability of scheme w/ speed of python
+       hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
